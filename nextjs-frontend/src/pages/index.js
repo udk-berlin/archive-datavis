65dde00288dc503d413010603ac02046a5163233
Robert Schnüll <mail@robertschnuell.de>
2024-11-03 20:50:24 +0100

    deployment and dev images

--- nextjs-frontend/src/pages/index.js
+++ nextjs-frontend/src/pages/index.js
@@ -3,6 +3,39 @@ import { Card } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
+
+  const dummyPerspectives= [
+    {
+      title: "new digital",
+      author: "Jussi Ängeslevä",
+      image: "/images/hero.jpeg"
+    },
+    {
+      title: "media renessaince",
+      author: "Jussi Ängeslevä",
+      image: "/images/3N7A0081.jpeg"
+    },
+    {
+      title: "the social web",
+      author: "Jussi Ängeslevä",
+      image: "/images/3N7A0084.jpeg"
+    },
+    {
+      title: "another era",
+      author: "Jussi Ängeslevä",
+      image: "/images/3N7A0078.jpeg"
+    },
+    {
+      title: "failed parthways",
+      author: "Jussi Ängeslevä",
+      image: "/images/3N7A0080.jpeg"
+    },
+    {
+      title: "some phyiscal",
+      author: "Jussi Ängeslevä",
+      image: "/images/3N7A0083.jpeg"
+    },
+    ]
   return (
     <>
       <section className="w-2/3 ">
@@ -18,14 +51,19 @@ export default function Home() {
         <h3 className="text-2xl">perspectives</h3>
         <p className="text-muted">lore lore</p>
         <Separator className="mt-4" />
-        <div className="mt-8 mb-12">
-          <div>
-            <Card className="w-[300px] h-[400px] rounded-xl overflow-hidden shadow-lg mb-4">
-              <img src="/images/hero.jpeg" className="w-full h-full object-cover" />
-            </Card>
-
-            <h4>new digital</h4>
-            <p className="text-muted italic">by Jussi Ängeslevä</p>
+        <div className="mt-8 mb-12 w-full overflow-x-auto">
+          <div className="flex space-x-10">
+            {dummyPerspectives.map((perspective, index) => (
+              <div key={index} className="flex-shrink-0">
+                <Card className="w-[300px] h-[400px] rounded-xl overflow-hidden shadow-lg">
+                  <img src={perspective.image} className="w-full h-full object-cover" />
+                </Card>
+                <div>
+                  <h4>{perspective.title}</h4>
+                  <p className="text-muted italic">by {perspective.author}</p>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </section>
