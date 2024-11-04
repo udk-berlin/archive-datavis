d76fecf8238dd0879e10d4dae585f6ff22c13f71
Robert Schnüll <mail@robertschnuell.de>
2024-11-04 20:43:11 +0100

    formatting

--- nextjs-frontend/src/components/layout/partials/HeaderNav.js
+++ nextjs-frontend/src/components/layout/partials/HeaderNav.js
@@ -3,13 +3,12 @@ import { RiLogoutBoxRLine, RiSearchLine } from "@remixicon/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-import { useTranslation } from 'react-i18next';
-
+import { useTranslation } from "react-i18next";
 
 const HeaderNav = () => {
   const router = useRouter();
   const currentPath = router.pathname;
-  const { t } = useTranslation('header'); 
+  const { t } = useTranslation("header");
   const { i18n } = useTranslation();
 
   return (
@@ -20,37 +19,45 @@ const HeaderNav = () => {
       <nav className="flex-grow ml-36">
         <ul className="flex space-x-24">
           <li>
-            <Link href="/archive" className={currentPath === "/archive" ? "!underline" : ""}>{t('Archive')}</Link>
+            <Link href="/archive" className={currentPath === "/archive" ? "!underline" : ""}>
+              {t("Archive")}
+            </Link>
           </li>
           <li>
-            <Link href="/perspectives" className={currentPath === "/perspectives" ? "!underline" : ""}>{t('Perspectives')}</Link>
+            <Link href="/perspectives" className={currentPath === "/perspectives" ? "!underline" : ""}>
+              {t("Perspectives")}
+            </Link>
           </li>
           <li>
-            <Link href="/contribute" className={currentPath === "/datanetwork" ? "!underline" : ""}>{t('Contribute')}</Link>
+            <Link href="/contribute" className={currentPath === "/datanetwork" ? "!underline" : ""}>
+              {t("Contribute")}
+            </Link>
           </li>
           <li>
-            <Link href="/about" className={currentPath === "/about" ? "!underline" : ""}>{t('About')}</Link>
+            <Link href="/about" className={currentPath === "/about" ? "!underline" : ""}>
+              {t("About")}
+            </Link>
           </li>
         </ul>
       </nav>
       <div className="flex">
         <RiLogoutBoxRLine className="mr-6 w-5" />
         <RiSearchLine className="mr-6 w-5" />
-      <Select className="ml-auto"  onValueChange={(e) => {
-                  i18n.changeLanguage(e);
-                }}>
-        <SelectTrigger className="w-auto border-none m-0 p-0 text-md pb-4" icon={false}>
-          <SelectValue placeholder={i18n.language} />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="en">en</SelectItem>
-          <SelectItem value="de">de</SelectItem>
-        </SelectContent>
-      </Select>
-
-     
+        <Select
+          className="ml-auto"
+          onValueChange={(e) => {
+            i18n.changeLanguage(e);
+          }}
+        >
+          <SelectTrigger className="w-auto border-none m-0 p-0 text-md pb-4" icon={false}>
+            <SelectValue placeholder={i18n.language} />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="en">en</SelectItem>
+            <SelectItem value="de">de</SelectItem>
+          </SelectContent>
+        </Select>
       </div>
-    
     </header>
   );
 };
