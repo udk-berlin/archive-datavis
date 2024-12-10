af82cd150ffd9c1e32592ced7ec333ae35861ee6
Robert Schnüll <mail@robertschnuell.de>
2024-12-10 12:39:53 +0100

    vis with dummyData

--- nextjs-frontend/src/pages/archive/sketch.js
+++ nextjs-frontend/src/pages/archive/sketch.js
@@ -8,44 +8,35 @@ import { v4 as uuidv4 } from "uuid";
 
 import Hud from "./Hud";
 
-
 export function sketch(p5) {
-
-  p5.constructor.prototype.screenPosition = function(x, y, z) {
+  p5.constructor.prototype.screenPosition = function (x, y, z) {
     const p = p5.createVector(x, y, z);
     const cam = p5._renderer._curCamera;
-  
+
     // Model-View-Projection matrix
-    const mvp = p5._renderer.uMVMatrix
-      .copy()
-      .mult(p5._renderer.uPMatrix);
-  
+    const mvp = p5._renderer.uMVMatrix.copy().mult(p5._renderer.uPMatrix);
+
     // Transform the point
     const coords = p4MultMatrix(p, mvp);
-  
+
     // Normalize
     const norm = coords.copy().div(coords.w);
-  
+
     // Map to screen coordinates
     norm.x = this.map(norm.x, -1, 1, 0, this.width);
     norm.y = this.map(-norm.y, -1, 1, 0, this.height);
-  
+
     return norm;
   };
-  
+
   function p4MultMatrix(p, m) {
     const result = p5.createVector();
-    result.x =
-      p.x * m.mat4[0] + p.y * m.mat4[4] + p.z * m.mat4[8] + m.mat4[12];
-    result.y =
-      p.x * m.mat4[1] + p.y * m.mat4[5] + p.z * m.mat4[9] + m.mat4[13];
-    result.z =
-      p.x * m.mat4[2] + p.y * m.mat4[6] + p.z * m.mat4[10] + m.mat4[14];
-    result.w =
-      p.x * m.mat4[3] + p.y * m.mat4[7] + p.z * m.mat4[11] + m.mat4[15];
+    result.x = p.x * m.mat4[0] + p.y * m.mat4[4] + p.z * m.mat4[8] + m.mat4[12];
+    result.y = p.x * m.mat4[1] + p.y * m.mat4[5] + p.z * m.mat4[9] + m.mat4[13];
+    result.z = p.x * m.mat4[2] + p.y * m.mat4[6] + p.z * m.mat4[10] + m.mat4[14];
+    result.w = p.x * m.mat4[3] + p.y * m.mat4[7] + p.z * m.mat4[11] + m.mat4[15];
     return result;
   }
-
 
   let img;
 
@@ -83,9 +74,9 @@ export function sketch(p5) {
   let updateFocusedIds = () => {};
   let updateFocusedType = () => {};
 
-  let parent 
+  let parent;
 
-  let canvasSizeChanged = false
+  let canvasSizeChanged = false;
 
   p5.updateWithProps = (props) => {
     if (props.windowWidth) {
@@ -103,20 +94,17 @@ export function sketch(p5) {
     if (props.autoRotation) {
       autoRotation = props.autoRotation === "on" ? true : false;
     }
-    if(props.canvasSizeChanged) {
-      canvasSizeChanged = props.canvasSizeChanged
-      if(windowWidth && parent) {
-        p5.resizeCanvas(windowWidth*(canvasSizeChanged/100), parent.offsetHeight,true);
-        p5.redraw()
-        p5.clear()
+    if (props.canvasSizeChanged) {
+      canvasSizeChanged = props.canvasSizeChanged;
+      if (windowWidth && parent) {
+        p5.resizeCanvas(windowWidth * (canvasSizeChanged / 100), parent.offsetHeight, true);
+        p5.redraw();
+        p5.clear();
       }
-      
     }
   };
 
-  async function resizeCanvasByParent() {
-
-  }
+  async function resizeCanvasByParent() {}
 
   p5.preload = async () => {
     img = p5.loadImage("/images/floatingShadow.png");
@@ -124,15 +112,13 @@ export function sketch(p5) {
     planetData = await fetchData("http://localhost:3010/api/all");
   };
 
-   p5.windowResized = () => {
- 
-    p5.resizeCanvas(parent.offsetWidth, parent.offsetHeight,true);
-  }
+  p5.windowResized = () => {
+    p5.resizeCanvas(parent.offsetWidth, parent.offsetHeight, true);
+  };
 
   p5.setup = async () => {
-     parent = document.getElementById("sketch-container");
+    parent = document.getElementById("sketch-container");
     p5.createCanvas(parent.offsetWidth, parent.offsetHeight, p5.WEBGL);
-
 
     hud = new Hud(p5);
 
@@ -149,26 +135,53 @@ export function sketch(p5) {
 
     await new Promise((r) => setTimeout(r, 400));
 
+    const dummyData = [...planetData.entries, ...planetData.entries];
+
+    const entriesDistance = 17
+    const entriesColumns = 10
 
     solarSystem.addPlanet(
       new Planet(p5, {
-        mode: "ring",
-        distance: planetData?.authors?.length*4 || 0,
+        mode: "plane",
+        distance: dummyData.length || 0,
         centralPoint: p5.createVector(0, 0, 0),
+        data: dummyData,
+        distance: entriesDistance,
+        rotationSpeed: 0.01,
+        id: "entries",
+        drawHull: false,
+        planeColumns: entriesColumns,
+      })
+    );
+
+
+    const dummyAuthors = [...planetData.authors, ...planetData.authors];
+
+    solarSystem.addPlanet(
+      new Planet(p5, {
+        mode: "stripe",
+        distance: dummyAuthors?.length * 4 || 0,
+        centralPoint: p5.createVector(0, ((dummyData.length/entriesColumns)*entriesDistance)/4, 0),
         rotationAngles: { angleX: 90, angleY: 0, angleZ: 0 },
-        data: planetData?.authors || [],
-      //  distance: 750,
+        data: dummyAuthors || [],
+          distance: 300,
         id: "authors",
+        stripeSettings: {
+          maxPerRing: 120,
+          layerDistance: 10,
+          layerRotation: 10,
+        },
+
       })
     );
 
     solarSystem.addPlanet(
       new Planet(p5, {
         mode: "ring",
-        distance: planetData?.semesters?.length*4 || 0,
-        centralPoint: p5.createVector(0, -500, 0),
+        distance: planetData?.semesters?.length * 4 || 0,
+        centralPoint: p5.createVector(0, ((dummyData.length/entriesColumns)*entriesDistance)/4*-1, 0),
         rotationAngles: { angleX: 90, angleY: 0, angleZ: 0 },
-       // distance: 500,
+         distance: 300,
         data: planetData?.semesters || [],
         id: "semesters",
       })
@@ -178,40 +191,28 @@ export function sketch(p5) {
       new Planet(p5, {
         mode: "stripe",
         distance: planetData?.archive?.length || 0,
-        centralPoint: p5.createVector(0, 500, 0),
+        centralPoint: p5.createVector(0, 0, 0),
         rotationAngles: { angleX: 90, angleY: 0, angleZ: 0 },
-       // distance: 500,
+         distance: 485,
         data: planetData?.archive || [],
         id: "archive",
+        stripeSettings: {
+          maxPerRing: 120,
+          layerDistance: 14,
+          layerRotation: 10,
+        }
       })
     );
 
-  
 
-    solarSystem.addPlanet(
-      new Planet(p5, {
-        mode: "plane",
-        distance: planetData?.entries?.length || 0,
-        centralPoint: p5.createVector(0, 0, 0),
-        data: planetData.entries ,
-        distance: 20,
-        rotationSpeed: 0.01,
-        id: "entries",
-        drawHull: false,
-        planeColumns: 14,
-      })
-    );
     p5.textFont(font);
     p5.textSize(32);
     p5.textAlign(p5.CENTER, p5.CENTER);
 
-
-
-    hud.addLabel({point:centralPoint, text:"Projects"});
-    hud.addLabel({point:p5.createVector(-1*planetData.authors.length*4,0, 0),text: "Persons"});
-    hud.addLabel({point:p5.createVector(-1*planetData.semesters.length*4,-500, 0),text: "Semesters"});
-    hud.addLabel({point:p5.createVector(-1*planetData.archive.length,500, 0), text:"Archive"});
-
+    hud.addLabel({ point: centralPoint, text: "Projects" });
+    hud.addLabel({ point: p5.createVector(-1 * planetData.authors.length * 4, 0, 0), text: "Persons" });
+    hud.addLabel({ point: p5.createVector(-1 * planetData.semesters.length * 4, -500, 0), text: "Semesters" });
+    hud.addLabel({ point: p5.createVector(-1 * planetData.archive.length, 500, 0), text: "Archive" });
   };
 
   function updateRotation() {
@@ -239,7 +240,7 @@ export function sketch(p5) {
     p5.background(236, 239, 241);
 
     //p5.ambientLight(150);
-   // p5.directionalLight(255, 255, 255, 1, 1, -1);
+    // p5.directionalLight(255, 255, 255, 1, 1, -1);
 
     if (autoRotation && p5.millis() > 2000) {
       updateRotation();
@@ -252,28 +253,21 @@ export function sketch(p5) {
     solarSystem.draw();
 
     // plane
-    // p5.fill(236, 239, 241)
-    // p5.plane(280,600)
-  
+    p5.fill(236, 239, 241)
+    p5.plane(200,1400)
 
     if (autoRotation && p5.millis() > 2000) p5.pop();
 
-    
-
     // const hoverIds = solarSystem.getHoverIds()
     // if(hoverIds && hoverIds.length > 0) {
-    //   hud.clearLabels() 
+    //   hud.clearLabels()
     //   const d = solarSystem.getPlanet(hoverIds[0]?.planetId)?.getPointById(hoverIds[0]?.id)
     //   console.log(hoverIds[0], d)
     //   hud.addLabel({point:d,text: "hover", type:"hover"});
     // }
 
- //    hud.draw(centralPoint);
-
-
+    //    hud.draw(centralPoint);
   };
-
-  
 
   p5.mouseClicked = () => {
     //solarSystem.setSingleIdActive()
